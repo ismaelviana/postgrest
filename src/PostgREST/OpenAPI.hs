0292f04f1a98ce4110541a17b8dfce2a555c5af7
Joe Nelson <cred+github@begriffs.com>
2016-08-12 09:45:37 -0700

    WIP: remove non-openapi root spec

--- src/PostgREST/OpenAPI.hs
+++ src/PostgREST/OpenAPI.hs
@@ -212,7 +212,7 @@ makeRootPathItem = ("/", p)
   where
     getOp = (mempty :: Operation)
       & tags .~ Set.fromList ["/"]
-      & produces ?~ makeMimeList [ApplicationJSON, OpenAPI]
+      & produces ?~ MimeList ["application/openapi+json; charset=utf-8"]
       & at 200 ?~ "OK"
     pr = (mempty :: PathItem) & get ?~ getOp
     p = pr
